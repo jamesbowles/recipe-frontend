3fa55e0a479470e119f284a8b2cc8ee9b90fc557
James Bowles <james.l.bowles@gmail.com>
2018-10-05 22:32:38 +1300

    upgrade to create-react-app 2, initial css grid

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
+import { Link } from "react-router-dom";
+
+import Header from './components/Header'
 import Recipe from './components/Recipe'
 import Recipes from './components/Recipes'
 import NewRecipe from './components/NewRecipe'
 import EditRecipe from './components/EditRecipe'
-import { Link } from "react-router-dom";
+
+import styles from './styles/App.module.css'
 
 const Home = () => (
-  <div className="ui container">
+  <div>
     <Recipes />
     <Link to={`/recipes/new`}>New recipe</Link>
-
   </div>
 );
 
 const App = () => (
   <Router>
-    <div>
-      <Route exact={true} path="/" component={Home} />
-      <Route exact path="/recipes/new" component={NewRecipe} />
-      <Route path="/recipes/show/:recipeId" component={Recipe} />
-      <Route path="/recipes/edit/:recipeId" component={EditRecipe} />
+    <div className={styles.App}>
+      <div className={styles.header}>
+        <Header />
+      </div>
+      <div className={styles.sidebar}>
+      </div>
+      <div className={styles.content}>
+        <Route exact={true} path="/" component={Home} />
+        <Route exact path="/recipes/new" component={NewRecipe} />
+        <Route path="/recipes/show/:recipeId" component={Recipe} />
+        <Route path="/recipes/edit/:recipeId" component={EditRecipe} />
+      </div>
     </div>
   </Router>
 )
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/Header.module.css'
+
+const Header = () => (
+  <div className={styles.Header}>
+    Recipes!
+  </div>
+);
+
+export default Header
